bd556d7e86d07fafaedd18d214dfa0e5641613e9
Xavier Trinidad <xmtrinidad2@gmail.com>
2019-02-17 15:24:13 -0700

    Initial commit

---
+++ src/components/app-nav.js
@@ -0,0 +1,77 @@
+import { LitElement, html, css } from 'lit-element';
+import { SharedStyles } from '../styles/shared-styles.js';
+import '@polymer/iron-icon'
+
+
+class AppNav extends LitElement {
+  static get properties() {
+    return {
+      testProp: { type: String },
+      navItems: { type: Array }
+    };
+  }
+
+  constructor() {
+    super();
+    this.navItems = ['Dashboard', 'Resources', 'Examples']
+  }
+
+  static get styles() {
+    return [
+      SharedStyles,
+      css`
+        :host {
+          display: block;
+        }
+
+        nav {
+          height: 64px;
+          display: flex;
+          justify-content: space-between;
+          align-items: center;
+          background-color: #3f51b5;
+          padding: 0 10px;
+        }
+
+        ul {
+          margin: 0;
+          padding: 0;
+          list-style: none;
+          display: flex;
+        }
+
+        iron-icon {
+          width: 36px;
+          height: 36px;
+          color: #fff;
+        }
+
+        ul li {
+          color: #fff;
+        }
+
+        ul li:not(:last-child) {
+          margin-right: 24px;
+        }
+      `
+    ];
+}
+
+  render(){
+    return html`
+      <nav>
+        <div>
+          <a href="#" aria-label="Home Page">
+            <iron-icon icon="accessibility"></iron-icon>
+          </a>
+        </div>
+        <ul>
+          ${this.navItems.map(i => html`<li><a href="#">${i}</a></li>`)}
+        </ul>
+      </nav>
+    `;
+  }
+}
+
+customElements.define('app-nav', AppNav);
+
---
+++ src/polymer-lit-app.js
@@ -0,0 +1,37 @@
+import { LitElement, html, css } from 'lit-element';
+
+import './components/app-nav.js';
+import './examples/docs-example.js';
+import '@polymer/iron-icons';
+import { SharedStyles } from './styles/shared-styles.js';
+
+/**
+ * @customElement
+ * @polymer
+ */
+class PolymerLitApp extends LitElement {
+  static get styles() {
+    return [
+      SharedStyles,
+      css`
+        :host {
+          display: block;
+        }
+      `
+    ];
+  }
+  static get properties() {
+    return {};
+  }
+
+  render() {
+    return html`
+      <app-nav></app-nav>
+      <main class="container">
+        <docs-example></docs-example>
+      </main>
+    `;
+  }
+}
+
+window.customElements.define('polymer-lit-app', PolymerLitApp);
